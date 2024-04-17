650f0a186749e35002d54227287a8ca9cb4e9516
Gustavo Henrique dos Santos <57962379+gustavo07henri@users.noreply.github.com>
2024-04-16 21:43:32 -0300

    Update Main.java

--- Lista Exercicios 06/Main.java
+++ Lista Exercicios 06/Main.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Main {
 
@@ -40,15 +41,62 @@ public static void main(String[] args) {
     }
 
     public static void Ex01 (Scanner input){
+        int[] vetor = new int[9];
+        int contador;
 
+
+        for (int i = 0; i < vetor.length; i++) {
+            System.out.print("Escreva um valor para o incluir no vetor: ");
+            vetor [i] = input.nextInt();
+        }
+        for (int j = 0; j < vetor.length; j++) {
+            contador = 0;
+            for(int i = 1; i < vetor.length; i++){
+                if(vetor[j] % i == 0){
+                    contador ++;
+                }
+                if(contador <= 2){
+                    System.out.print("O número "+ vetor[j]+" de posição "+j+" é primo.");
+                }
+            }
+        }
+      
+           
+        
     }
     public static void Ex02 (Scanner input){
+        int[] vetorUm = new int[10];
+        int[] vetorDois = new int[10];
+        int[] intercalado = new int[20];
 
+        for (int i = 0; i < vetorUm.length; i++) {
+            System.out.print("Insira o "+i+"° valor para o 1° Vetor.");
+            vetorUm[i] = input.nextInt();
+        }
+        for (int i = 0; i < vetorDois.length; i++) {
+            System.out.print("Insira o "+i+"° valor para o 2° Vetor.");
+            vetorDois[i] = input.nextInt();
+        }
+       
+        for (int i : vetorUm) {
+            int j = 0;
+            intercalado[j] = i;
+            j += 2;
+        }    
+        for (int i : vetorDois) {
+            int a = 1;
+            intercalado[a] = i;
+            a += 2;
+        }    
+        for (int i = 0; i < intercalado.length; i++) {
+            System.out.println(intercalado[i]);
+        }
     }
+    
     public static void Ex03 (Scanner input){
 
     }
     public static void Ex04 (Scanner input){
-
+        
     }
 }
